ef81daddef368bdfd7277ab8617bd79010fc8b38
Gastón Ginestet <gastonauginestet@gmail.com>
2021-02-08 01:15:05 -0300

    users redirect fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(:users, notice: 'User was successfully created')
+      redirect_to(:pages, notice: 'User was successfully created')
     else
       render :new
     end
